dd8fcb44272b08972f9b7b3fca3e49453b579152
Tom Cahill <me@tomcahill.co.uk>
2022-04-05 10:01:13 +0100

    MERGE REFACTOR: Update Licencing

--- bin/app-socket.js
+++ bin/app-socket.js
@@ -2,13 +2,19 @@
 'use strict';
 
 /**
- * ButtressJS - Realtime datastore for software
+ * Buttress - The federated real-time open data platform
+ * Copyright (C) 2016-2022 Data Performance Consultancy LTD.
+ * <https://dataperformanceconsultancy.com/>
  *
- * @file socket-app.js
- * @description Entry point for socket app
- * @module Model
- * @author Chris Bates-Keegan
- *
+ * This file is part of Buttress.
+ * Buttress is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public Licence as published by the Free Software
+ * Foundation, either version 3 of the Licence, or (at your option) any later version.
+ * Buttress is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public Licence for more details.
+ * You should have received a copy of the GNU Affero General Public Licence along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 const env = (process.env.ENV_FILE) ? process.env.ENV_FILE : process.env.NODE_ENV;
