80101d3c48191e0527184c1de2e8ea730985ee94
Tom Cahill <me@tomcahill.co.uk>
2026-05-06 17:01:05 +0100

    REFACTOR: No more queryString tokens

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -630,7 +630,7 @@ class Routes {
 
   async _getProvidedToken(req) {
     // Get the bearer token from the Authorization header or query string
-    let tokenValue: string | undefined = req.headers['authorization'] || req.query.token;
+    let tokenValue: string | undefined = req.headers['authorization'];
     if (tokenValue) tokenValue = tokenValue.replace('Bearer ', '');
 
     Logging.logSilly(`_getProvidedToken:start ${tokenValue}`, req.id);
